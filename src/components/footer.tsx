c6e683aee3c649b8371fd5474e6bd3568b3fb3dc
KhawlaRajab <khawlarajab23@gmail.com>
2025-01-21 01:21:20 +0200

    add footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,20 +1,19 @@
-import { Box, styled } from "@mui/material";
+import { Box, Stack, Typography, styled } from "@mui/material";
 
-// const StyledBox = styled(Box)({
-//     height: '200px',
-//     backgroundColor:'gainsboro'
-// })
 const StyledBox = styled(Box)({
-    marginTop:'20px',
+    marginTop:'30px',
     width: '100%',
-    height: '200px',
-    backgroundColor:'ghostwhite'
+    height: '150px',
+    backgroundColor: 'ButtonFace',
 })
 
 const Footer: React.FC = () => {
     return (
         <StyledBox >
-            
+            <Stack direction={'column'} alignItems={'center'} justifyContent={'center'} paddingTop={8}>
+                <Typography variant="body2">TravelZ the best platform to book hotel for your trip</Typography>
+                 <Typography variant="body2">@TravelZ 2025</Typography>
+           </Stack>    
         </StyledBox>
     )
 }
