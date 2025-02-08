bbc0a3c2373297c2f56f5e1b2088dfc1fbe2b42f
KhawlaRajab <khawlarajab23@gmail.com>
2025-02-08 15:55:29 +0200

    indicated available and unavailable rooms

--- src/pages/HotelPage/Room.tsx
+++ src/pages/HotelPage/Room.tsx
@@ -1,4 +1,4 @@
-import { Card, CardMedia, CardContent, Typography, Button, Stack } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, Button, Stack, Box, styled } from "@mui/material";
 import { room } from "./type";
 import { useCart } from "../CheckoutPage/useCart";
 
@@ -7,6 +7,24 @@ interface roomProps{
   inCart:boolean
   
 }
+const StyledBox = styled(Box)({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+  color: 'white',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize:'25px',
+  fontWeight: 'bold',
+  textTransform: 'uppercase',
+  zIndex: 1,  
+  
+})
+
 
 const Room: React.FC<roomProps> = ({ room,inCart=false }) => {
   const cartContext = useCart();
@@ -14,7 +32,8 @@ const Room: React.FC<roomProps> = ({ room,inCart=false }) => {
   const { addToCart, checkItem , removeItem} = cartContext;
 
     return (
-        <Card sx={{ maxWidth: 335, boxShadow: 3 ,borderRadius:2 , minHeight:420}}>
+        <Card sx={{ maxWidth: 335, boxShadow: 3, borderRadius: 2, minHeight: 450, position:'relative' }}>
+        {!room.availability && <StyledBox>Unavailable Room</StyledBox>} 
         <CardMedia
           component="img"
           height="200"
